9ed7bafdb02d19e83b0ba1f9230043cff14bc0c4
Ethikry <113566829+Ethikry@users.noreply.github.com>
2023-02-25 03:10:11 -0700

    Finish creating models, adding associations. Finished categories routes

--- 13-Challenge/Develop/routes/api/category-routes.js
+++ 13-Challenge/Develop/routes/api/category-routes.js
@@ -1,28 +1,84 @@
-const router = require('express').Router();
-const { Category, Product } = require('../../models');
+const router = require("express").Router();
+const { Category, Product } = require("../../models");
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll({
+    attributes: ["id", "category_name"],
+    include: [
+      {
+        model: Product,
+        attributes: ["id", "product_name", "price", "stock", "category_id"],
+      },
+    ],
+  }).then(function (data) {
+    (err, res) => {
+      if (err) throw err;
+      res.JSON(data);
+    };
+  });
 });
 
-router.get('/:id', (req, res) => {
+router.get("/:id", (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findByPk(req.params.id, {
+    attributes: ["id", "category_name"],
+    include: [
+      {
+        model: Product,
+        attributes: ["id", "product_name", "price", "stock", "category_id"],
+      },
+    ],
+  }).then(function (data) {
+    (err, res) => {
+      if (err) throw err;
+      res.JSON(data);
+    };
+  });
 });
 
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   // create a new category
+  Category.create(req.body).then(function (data) {
+    (err, res) => {
+      if (err) throw err;
+      res.status(200).JSON(data);
+    };
+  });
 });
 
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   // update a category by its `id` value
+  Category.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then(Category.findByPk(req.params.id))
+    .then(function (data) {
+      (err, res) => {
+        if (err) throw err;
+        res.status(200).JSON(data);
+      };
+    });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id,
+    },
+  }).then(function (data) {
+    (err, res) => {
+      if (err) throw err;
+      res.JSON(`${data} was destroyed.`);
+    };
+  });
 });
 
 module.exports = router;
